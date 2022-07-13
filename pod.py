044f516c0c639b9141aabe2bc26cb9ba562fec5c
Sigmoid <kchaithanya972@gmail.com>
2022-07-13 15:26:20 +0530

    intial one

---
+++ pod.py
@@ -0,0 +1,13 @@
+import psutil
+import sched, time
+print ("Hello World")
+s=sched.scheduler(time.time,time.sleep)
+def do_something(sc):
+   cpu=psutil.cpu_percent(interval=10)
+   mem=psutil.virtual_memory()
+   print(f'CPU:{cpu}')
+   print(f'Memory:{mem}')
+   s.enter(2,1,do_something,(sc,))
+s.enter(2,1,do_something,(s,))
+s.run()
+
